07c1709ebc7425ae45f4e9fbe15be639f2f495bb
ek121268 <erank@perfectomobile.com>
2016-11-21 15:55:08 -0500

    Update AndroidManifestReader.java

--- AndroidManifestReader.java
+++ AndroidManifestReader.java
@@ -6,8 +6,10 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.StringReader;
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
 
@@ -17,6 +19,8 @@ import javax.xml.parsers.ParserConfigurationException;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
@@ -28,11 +32,17 @@ public class AndroidManifestReader {
 	public AndroidManifestReader(String filePath) throws FileNotFoundException, IOException, ParserConfigurationException, SAXException {
 		byte[] buf = readFile(filePath);
 		String xml = AndroidXMLDecompress.decompressXML(buf);
+		System.out.println(xml);
 		_document = getDocument(xml);
 	}
 
 	public List<String> getUsesFeatures() {
 		List<String> usesFeatures = getElementNames("uses-feature");
+		return usesFeatures;
+	}
+	
+	public Map<String, String> getUsesSdk() {
+		Map<String, String> usesFeatures = getElementAttributes("uses-sdk");
 		return usesFeatures;
 	}
 	
@@ -49,6 +59,21 @@ public class AndroidManifestReader {
 	public Document getDocument() {
 		return _document;
 	}
+	
+	private Map<String, String> getElementAttributes(String path) {
+		NodeList nodes = _document.getElementsByTagName(path);
+		Map<String,String> elementAttributes = new HashMap<>();
+		if (nodes != null && nodes.getLength() != 0) {
+			Element element = (Element) nodes.item(0);
+			NamedNodeMap attributes = element.getAttributes();
+			for (int i = 0; i < attributes.getLength(); i++) {
+				Node attribute = attributes.item(i);
+				elementAttributes.put(attribute.getNodeName(), attribute.getNodeValue());
+			}
+		}
+		return elementAttributes;
+	}
+
 	
 	private List<String> getElementNames(String path) {
 		NodeList nodes = _document.getElementsByTagName(path);
@@ -112,7 +137,7 @@ public class AndroidManifestReader {
 	}
 
 	public static void main(String[] args) throws IOException, ParserConfigurationException, SAXException {
-		String filePath = "C:\\aaa\\apps\\testMoveLongPath.apk";
+		String filePath = "C:\\Users\\erank\\Downloads\\1939680295-angry-birds-2.apk";
 //		String filePath = "C:\\aaa\\apps\\1939680295-angry-birds-2.apk";		
 		AndroidManifestReader reader = new AndroidManifestReader(filePath);
 		
@@ -124,8 +149,12 @@ public class AndroidManifestReader {
 		
 		List<String> activities = reader.getActivities();
 		System.out.println("Activities:" + activities);
+		
+		Map<String, String> usesSdk = reader.getUsesSdk();
+		System.out.println("SDK: " + usesSdk);
 
 
 	}
 
 }
+
